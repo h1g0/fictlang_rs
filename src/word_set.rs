d88f98b0810515da12d203a06079cc46af78c716
hi_go <me@clutte.red>
2021-08-22 09:18:49 +0900

    Implement `word_set`.

--- src/word_set.rs
+++ src/word_set.rs
@@ -1,18 +1,56 @@
-
 pub struct Prefix {
     pub conjugation: Vec<String>,
+}
+impl Prefix {
+    pub fn new(conjugation: Vec<String>) -> Prefix {
+        Prefix { conjugation }
+    }
 }
 pub struct PrefixSet {
     pub prefixes: Vec<Prefix>,
 }
+impl PrefixSet {
+    pub fn new(prefixes: Vec<Prefix>) -> PrefixSet {
+        PrefixSet { prefixes }
+    }
+}
+
 pub struct Suffix {
     pub conjugation: Vec<String>,
+}
+impl Suffix {
+    pub fn new(conjugation: Vec<String>) -> Suffix {
+        Suffix { conjugation }
+    }
 }
 pub struct SuffixSet {
     pub suffixes: Vec<Suffix>,
 }
+impl SuffixSet {
+    pub fn new(suffixes: Vec<Suffix>) -> SuffixSet {
+        SuffixSet { suffixes }
+    }
+}
+
 pub struct Word {
     prefix_idx: Option<usize>,
     suffix_idx: Option<usize>,
     str: String,
+}
+impl Word {
+    pub fn new(prefix_idx: Option<usize>, suffix_idx: Option<usize>, str: String) -> Word {
+        Word {
+            prefix_idx,
+            suffix_idx,
+            str,
+        }
+    }
+}
+pub struct WordSet {
+    pub words: Vec<Word>,
+}
+impl WordSet {
+    pub fn new(words: Vec<Word>) -> WordSet {
+        WordSet { words }
+    }
 }
